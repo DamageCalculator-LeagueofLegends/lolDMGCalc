d05fdb5239324f56795d643603a93b29f7a1dddb
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-10-17 11:11:55 +0200

    Initial Commit

---
+++ lolDMGCalc/lolDMGCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  lolDMGCalcApp.swift
+//  lolDMGCalc
+//
+//  Created by Laurin Notemann on 17.10.22.
+//
+
+import SwiftUI
+
+@main
+struct lolDMGCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
